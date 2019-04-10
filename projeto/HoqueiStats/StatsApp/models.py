bae47f870483030151d68ad05b16f2d24d519b4d
SaleteTeixeira <salete.diasteixeira@hotmail.com>
2019-04-10 18:41:11 +0100

    corrigidos models

--- projeto/HoqueiStats/StatsApp/models.py
+++ projeto/HoqueiStats/StatsApp/models.py
@@ -5,6 +5,12 @@ from django.db import models
 from django.utils import timezone
 
 
+class Admin(models.Model):
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    def __str__(self):
+        return self.email
+
 class Clube(models.Model):
     nome = models.CharField(max_length=200)
     cor = models.CharField(max_length=6)
@@ -43,6 +49,8 @@ class Tecnico(models.Model):
     email = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
     clube = models.ForeignKey(Clube, on_delete=models.CASCADE)
+    grelhaCampo = models.CharField(max_length=5)
+    grelhaBaliza = models.CharField(max_length=5)
     def __str__(self):
         return self.nome
     class Meta:
@@ -63,23 +71,39 @@ class Jogo(models.Model):
 class Convocado(models.Model):
     atleta = models.ForeignKey(Atleta, on_delete=models.CASCADE)
     jogo = models.ForeignKey(Jogo, on_delete=models.CASCADE)
-    emcampo = models.BooleanField(default=True)
+    emCampo = models.BooleanField(default=True)
     def __str__(self): 
         return self.jogo
 
+class TipoEvento(models.Model):
+    tipo = models.CharField(max_length=200)
+    atleta1 = models.BooleanField(default=True)
+    atleta2 = models.BooleanField(default=True)
+    zonaCampo = models.BooleanField(default=True)
+    zonaBaliza = models.BooleanField(default=True)
+    novoinstante = models.BooleanField(default=True)
+    def __str__(self):
+        return self.tipo
+
+class TiposSelecionados(models.Model):
+    tecnico = models.ForeignKey(Tecnico, on_delete=models.CASCADE)
+    tipo = models.ForeignKey(TipoEvento, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.tecnico
+
 class Evento(models.Model):
-   tipo = models.CharField(max_length=200)
-   jogo = models.ForeignKey(Jogo, on_delete=models.CASCADE)
-   equipa = models.ForeignKey(Formacao, on_delete=models.CASCADE)
-   atleta1 = models.ForeignKey(Atleta, on_delete=models.CASCADE, related_name='AtletaEntra', blank=True, null=True)
-   atleta2 = models.ForeignKey(Atleta, on_delete=models.CASCADE, related_name='AtletaSai', blank=True, null=True)
-   zonaCampo = models.IntegerField(default=None, blank=True, null=True)
-   zonaBaliza = models.IntegerField(default=None, blank=True, null=True)
-   instante = models.TimeField()
-   novoinstante = models.TimeField(default=None, blank=True, null=True)
-   timestamp = models.DateTimeField(auto_now_add=True)
-   def __str__(self):
-       return self.tipo
+    tipo = models.ForeignKey(TipoEvento, on_delete=models.CASCADE)
+    jogo = models.ForeignKey(Jogo, on_delete=models.CASCADE)
+    equipa = models.ForeignKey(Formacao, on_delete=models.CASCADE)
+    atleta1 = models.ForeignKey(Atleta, on_delete=models.CASCADE, related_name='AtletaEntra', blank=True, null=True)
+    atleta2 = models.ForeignKey(Atleta, on_delete=models.CASCADE, related_name='AtletaSai', blank=True, null=True)
+    zonaCampo = models.IntegerField(default=None, blank=True, null=True)
+    zonaBaliza = models.IntegerField(default=None, blank=True, null=True)
+    instante = models.TimeField()
+    novoinstante = models.TimeField(default=None, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.tipo
 
 
 
