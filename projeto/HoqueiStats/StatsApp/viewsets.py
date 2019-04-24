e3968a907db0999fb1a5ac80ceeb1a4d3b54994e
Daniel Maia <33250763+SilentLynx@users.noreply.github.com>
2019-04-24 16:15:30 +0100

    suporte rest adicionado aos modelos, métodos de acesso à api para atletas adicinoado

--- projeto/HoqueiStats/StatsApp/viewsets.py
+++ projeto/HoqueiStats/StatsApp/viewsets.py
@@ -1,7 +1,43 @@
 from rest_framework import viewsets
-from .models import Clube
-from .serializers import ClubeSerializer
+from .models import Clube, Atleta
+from .serializers import *
 
 class ClubeViewSet(viewsets.ModelViewSet):
     queryset = Clube.objects.all()
     serializer_class = ClubeSerializer
+
+class FormacaoViewSet(viewsets.ModelViewSet):
+    queryset = Formacao.objects.all()
+    serializer_class = FormacaoSerializer
+
+class AtletaViewSet(viewsets.ModelViewSet):
+    queryset = Atleta.objects.all()
+    serializer_class = AtletaSerializer
+
+class GestorViewSet(viewsets.ModelViewSet):
+    queryset = Gestor.objects.all()
+    serializer_class = GestorSerializer
+
+class TecnicoViewSet(viewsets.ModelViewSet):
+    queryset = Tecnico.objects.all()
+    serializer_class = TecnicoSerializer
+
+class JogoViewSet(viewsets.ModelViewSet):
+    queryset = Jogo.objects.all()
+    serializer_class = JogoSerializer
+
+class ConvocadoViewSet(viewsets.ModelViewSet):
+    queryset = Convocado.objects.all()
+    serializer_class = ConvocadoSerializer
+
+class TipoEventoViewSet(viewsets.ModelViewSet):
+    queryset = TipoEvento.objects.all()
+    serializer_class = TipoEventoSerializer
+
+class TiposSelecionadosViewSet(viewsets.ModelViewSet):
+    queryset = TiposSelecionados.objects.all()
+    serializer_class = TiposSelecionadosSerializer
+
+class EventoViewSet(viewsets.ModelViewSet):
+    queryset = Evento.objects.all()
+    serializer_class = EventoSerializer
